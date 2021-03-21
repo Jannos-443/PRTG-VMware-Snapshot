4501031937ff0aabf7235788020fb3bb2468d87f
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2021-03-21 12:08:40 +0100

    Update PRTG-VMware-Snapshot.ps1

--- PRTG-VMware-Snapshot.ps1
+++ PRTG-VMware-Snapshot.ps1
@@ -1,18 +1,16 @@
-<#       
+<#   
     .SYNOPSIS
     Monitors Snapshot Age/Size
 
     .DESCRIPTION
-    Using PowerCLI this script searches for VMware Snapshots.
+    Using WMI this script searches for pending Print Jobs.
     Exceptions can be made within this script by changing the variable $IgnoreScript. This way, the change applies to all PRTG sensors 
     based on this script. If exceptions have to be made on a per sensor level, the script parameter $IgnorePattern can be used.
 
-    Copy this script to the PRTG probe EXE scripts folder (${env:ProgramFiles(x86)}\PRTG Network Monitor\Custom Sensors\EXE)
-    and create a "EXE/Script" sensor. Choose this script from the dropdown and set at least:
+    Copy this script to the PRTG probe EXEXML scripts folder (${env:ProgramFiles(x86)}\PRTG Network Monitor\Custom Sensors\EXEXML)
+    and create a "EXE/Script Advanced. Choose this script from the dropdown and set at least:
 
     + Parameters: VCenter, Username, Password
-    + Upper Error Limit 1
-    + Upper Warning Limit 0,1
     + Scanning Interval: minimum 5 minutes
 
     .PARAMETER ViServer
@@ -29,7 +27,7 @@
 
     Example: ^(DemoTestServer|DemoAusname2)$
 
-    Example2: ^(Test123.*|TestPrinter555)$ excludes Test123 and also Test12345
+    Example2: ^(Test123.*|TestPrinter555)$ excluded Test12345 und alles mit 
 
     #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_regular_expressions?view=powershell-7.1
     
@@ -46,9 +44,8 @@
     Errorlimit for Snapshot Size
     
     .EXAMPLE
-    Sample call from PRTG EXE/Script
-    EXE/Script= PSx64.exe
-    Parameters= -f="PRTG-VMware-Snapshot.ps1" -p="%VCenter%" "%Username%" "%PW%" "(DemoTestServer|DemoAusname2)"
+    Sample call from PRTG EXE/Script Advanced
+    PSx64.exe -f="PRTG-VMware-Snapshot.ps1" -p="%VCenter%" "%Username%" "%PW%"
 
     .NOTES
     This script is based on the sample by Paessler (https://kb.paessler.com/en/topic/67869-auto-starting-services) and debold (https://github.com/debold/PRTG-WindowsServices)
@@ -61,14 +58,24 @@ param(
     [Parameter(Mandatory)] [string]$ViServer = $null,
     [Parameter(Mandatory)] [string]$User = $null,
     [Parameter(Mandatory)] [string]$Password = $null,
-    [string]$IgnorePattern = "",
+    [string]$IgnorePattern = "", #VMs to ignore
     [int]$WarningHours = 24,
     [int]$ErrorHours = 48,
     [int]$WarningSize = 10,  #in GB
     [int]$ErrorSize = 20     #in GB
 )
 
-try{
+#Catch all unhandled Errors
+trap{
+    $null = Disconnect-VIServer -Server $ViServer -Confirm:$false -ErrorAction SilentlyContinue
+    Write-Output "<prtg>"
+    Write-Output " <error>1</error>"
+    Write-Output " <text>$($_.ToString() - $($_.ScriptStackTrace))</text>"
+    Write-Output "</prtg>"
+    Exit
+}
+
+
 $WarningVMs = ""
 $ErrorVMs = ""
 
@@ -82,8 +89,11 @@ $ViModule = "VMware.VimAutomation.Core"
 try {
     Import-Module $ViModule -ErrorAction Stop
 } catch {
-    Write-Host "Error Loading VMware Powershell Module ($($_.Exception.Message))"
-    Exit 2
+    Write-Output "<prtg>"
+    Write-Output " <error>1</error>"
+    Write-Output " <text>Error Loading VMware Powershell Module ($($_.Exception.Message))</text>"
+    Write-Output "</prtg>"
+    Exit
 }
 
 # Parameter empty = 999
@@ -110,8 +120,11 @@ Set-PowerCLIConfiguration -InvalidCertificateAction ignore -confirm:$false | Out
 try {
     Connect-VIServer -Server $ViServer -User $User -Password $Password -ErrorAction Stop | Out-Null
 } catch {
-    Write-Host "Could not connect to vCenter server $ViServer. Error: $($_.Exception.Message)"
-    Exit 2
+    Write-Output "<prtg>"
+    Write-Output " <error>1</error>"
+    Write-Output " <text>Could not connect to vCenter server $ViServer. Error: $($_.Exception.Message)</text>"
+    Write-Output "</prtg>"
+    Exit
 }
 
 # Get a list of all VMs
@@ -119,8 +132,11 @@ try {
     $VMs = Get-VM -ErrorAction Stop
 
 } catch {
-    Write-Host "Could in Get-VM. Error: $($_.Exception.Message)"
-    Exit 2
+    Write-Output "<prtg>"
+    Write-Output " <error>1</error>"
+    Write-Output " <text>Could in Get-VM. Error: $($_.Exception.Message)</text>"
+    Write-Output "</prtg>"
+    Exit
 }
 
 
@@ -136,7 +152,7 @@ foreach ($VM in $VMs) {
 
 #Filter Snapshots
 
-#hardcoded list that applies to all hosts
+# hardcoded list that applies to all hosts
 $IgnoreScript = '^(TestIgnore)$' 
 
 #Remove Ignored VMs
@@ -148,48 +164,68 @@ if ($IgnoreScript -ne "") {
     $AllSnaps = $AllSnaps | where {$_.VM -notmatch $IgnoreScript}  
 }
 
+$WarningCount = 0
+$ErrorCount = 0
+$AllCount = $AllSnaps.Count
 
 foreach ($Snap in $AllSnaps){
     if(($Snap.Size -ge $ErrorSize) -or ($Snap.Created -le (get-date).AddHours(-$ErrorHours)))
         {
         $ErrorVMs += "VM=$($Snap.VM) Created=$(($snap.Created).ToString("yy-MM-dd_HH-mm")) Size=$([math]::Round(($Snap.SizeGB),2))GB; "
+        $ErrorCount +=1
         }
     if(($Snap.Size -ge $WarningSize) -or ($Snap.Created -le (get-date).AddHours(-$WarningHours)))
         {
         $WarningVMs  += "VM=$($Snap.VM) Created=$(($snap.Created).ToString("yy-MM-dd_HH-mm")) Size=$([math]::Round(($Snap.SizeGB),2))GB; " 
+        $WarningCount +=1
         }
-        
-
-
+       
 }
 
 
 # Disconnect from vCenter
 Disconnect-VIServer -Server $ViServer -Confirm:$false
 
-}
-
-catch{
-    Disconnect-VIServer -Server $ViServer -Confirm:$false -ErrorAction SilentlyContinue
-    Write-Host "Script Failed"
-    Exit 2
-    }
-
-
 
 # Results
-if ($ErrorVMs -ne "") {
-    Write-Host "2:Error - $($ErrorVMs)"
-    # Write-Host "Error - $($ErrorVMs)"  #Will Display the Message but "No data" and default says Error after two Times no Data...
-    exit 2
+$xmlOutput = '<prtg>'
+if ($ErrorCount -ge 1) {
+    $xmlOutput = $xmlOutput + "<text>Error - $($ErrorVMs)</text>"
     }
 
-if ($WarningVMs -ne ""){
-    Write-Host "1:Warning - $($WarningVMs)"
-    exit 1
+if (($WarningCount -ge 1) -and ($ErrorCount -eq 0)){
+    $xmlOutput = $xmlOutput + "<text>Warning - $($WarningVMs)</text>"
     } 
 
-else {
-    Write-Host "0:No Snapshots older $($WarningHours) hours or greater $($WarningSize)GB"
-    exit 0
+if(($WarningCount -eq 0) -and ($ErrorCount -eq 0)) {
+    $xmlOutput = $xmlOutput + "<text>No Snapshots older $($WarningHours) hours or greater $($WarningSize)GB</text>"
 }
+
+
+$xmlOutput = $xmlOutput + "<result>
+        <channel>Total Snapshots</channel>
+        <value>$AllCount</value>
+        <unit>Count</unit>
+        </result>
+        <result>
+        <channel>Snapshot Error</channel>
+        <value>$ErrorCount</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0.1</LimitMaxError>
+        </result>
+        
+        <result>
+        <channel>Snapshot Warning</channel>
+        <value>$WarningCount</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxWarning>0.1</LimitMaxWarning>
+        </result>"   
+        
+
+
+
+$xmlOutput = $xmlOutput + "</prtg>"
+
+$xmlOutput
